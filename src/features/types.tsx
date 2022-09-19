1f6241ac2e80714a3b1b3671fba255459f2c422f
petermalinovskiy <petermalinovskiy@gmail.com>
2022-09-19 11:33:42 +0300

    Delete not used files

--- src/features/types.tsx
+++
@@ -1,8 +0,0 @@
-export type RootStackParamList = {
-  EnterScreen: undefined;
-  Login: undefined;
-  Registration: undefined;
-  Main: undefined,
-  Cafe: {cafeData: {id: string, name: string, address: string, coordinates: string, description: string, images: string,}}
-  Drink: undefined
-};
